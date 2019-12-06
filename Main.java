089149cedbc03033966c85e0df86381d0f7f45ed
Adrianicko213 <58575709+Adrianicko213@users.noreply.github.com>
2019-12-06 09:06:21 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,7 +1,22 @@
+
+package main;
+import java.util.*;
 public class Main {
 
-  public static void main(String[] args) {
-    System.out.println("Hello World");
-  }
-
+   
+    public static void main(String[] args) {
+       Scanner scn = new Scanner(System.in);
+       System.out.println("What's your name?");
+       String name = scn.nextLine();
+       System.out.println("What's your address?");
+       String address = scn.nextLine();
+       System.out.println("How old are you?");
+       int age = scn.nextInt();
+      
+       System.out.println(" Hi " + name);
+       System.out.println("You live in " + address);
+       System.out.println("You are " + age);
+       
+    }
+    
 }
